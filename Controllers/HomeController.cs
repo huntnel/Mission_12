6979acb3eb5399573749bd0d144c0562b72888a4
Hunter Nelson <huntnel3@byu.edu>
2022-03-21 13:16:58 -0600

    Fixed Model errors

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace Mission_12.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult SignUp()
         {
             return View();
         }
--- Models/TimeSlotContext.cs
+++ Models/TimeSlotContext.cs
@@ -8,6 +8,14 @@ namespace Mission_12.Models
 {
     public class TimeSlotContext : DbContext
     {
+
+        public TimeSlotContext()
+        {
+        }
+        public TimeSlotContext(DbContextOptions<TimeSlotContext> options)
+            : base(options)
+        {
+        }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TimeSlot>()
@@ -15,5 +23,19 @@ namespace Mission_12.Models
         }
 
         public virtual DbSet<TimeSlot> TimeSlots { get; set; }
+
+        /*internal class MyContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+
+            #region Required
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<Blog>()
+                    .Property(b => b.Url)
+                    .IsRequired();
+            }
+            #endregion
+        }*/
     }
 }
