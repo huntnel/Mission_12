db826167bbb658a7e1638fb93a0a1977dee7c5fb
Buscompany21 <buscobrian1@gmail.com>
2022-03-22 10:24:48 -0600

    Coded up the controller to route for edit /delete/ get/post

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace Mission_12.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private AppointmentsContext _aptContext { get; set; }
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -23,9 +24,60 @@ namespace Mission_12.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult SignUp()
         {
+            ViewBag.Appointments = _aptContext.Appointments.ToList();
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(Appointment a)
+        {
+            if (ModelState.IsValid)
+            {
+                _aptContext.Add(a);
+                _aptContext.SaveChanges();
+
+                return View("Home", a);
+            }
+            else
+            {
+                ViewBag.Appointments = _aptContext.Appointments.ToList();
+                return View(a);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int appointmentId)
+        {
+            ViewBag.Categories = _aptContext.Appointments.ToList();
+            var aptEntry = _aptContext.Appointments.Single(x => x.AppointmentId == appointmentId);
+            return View("SignUp", aptEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Appointment a)
+        {
+            _aptContext.Update(a);
+            _aptContext.SaveChanges();
+
+            return RedirectToAction("Appointments");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int appointmentId)
+        {
+            var aptEntry = _aptContext.Appointments.Single(x => x.AppointmentId == appointmentId);
+            return View(aptEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Appointment a)
+        {
+            _aptContext.Appointments.Remove(a);
+            _aptContext.SaveChanges();
+            return RedirectToAction("Appointments");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
